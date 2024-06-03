d2f19be77fbee15c2c5781ff039793c98509c021
farouk <farouksouei@gmail.com>
2024-06-03 12:49:46 +0100

    fisrt version 2

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 
 class HomeController extends Controller
 {
@@ -14,6 +17,35 @@ class HomeController extends Controller
      */
     public function __invoke(Request $request)
     {
+        // get request data
+        $data = $request->all();
+
         return inertia('Home');
+    }
+
+
+    public function store(Request $request): \Illuminate\Routing\Redirector|\Illuminate\Contracts\Foundation\Application|\Illuminate\Http\RedirectResponse
+    {
+        $validated = $request->validate([
+            'name' => ['required'],
+            'username' => ['required'],
+            'email' => ['required','unique:users'],
+            'password' => ['required'],
+        ]);
+
+        if($validated) {
+            $newUser = User::create($validated);
+
+            Auth::login($newUser);
+
+            return redirect('/dashboard')->with([
+                'type' => 'success',
+                'message' => 'You are logged in.'
+            ]);
+        }
+
+        throw ValidationException::withMessages([
+            'email' => 'The provide credentials does not match our record.',
+        ]);
     }
 }
